c6313039f5a563324114d9127bc0cb6afd697dcc
Faizieva Venera <v.faizyeva@gmail.com>
2023-01-03 16:37:34 +0300

    fix: deleted array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 module.exports = function toReadable (number) {
-
     let arr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen','fourteen'
     ,'fifteen', 'sixteen','seventeen','eighteen', 'nineteen']
     let decade = ['twenty','thirty','forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-    let hundred = ['hundred']
     
       if (number==0){
         return 'zero';
@@ -16,7 +14,7 @@ module.exports = function toReadable (number) {
         }else{
         return `${readDecade(Number(String(number)[0]))} ${read(Number(String(number)[1]))}`;
       }
-      }else if(number>=100){
+      }else if(number>=100 && number<1000){
         if(String(number).slice(-2)=='00'){
         return `${read(Number(String(number)[0]))} hundred`;
       }else if(+String(number).slice(1)<20){
@@ -27,7 +25,6 @@ module.exports = function toReadable (number) {
         return `${read(Number(String(number)[0]))} hundred ${readDecade(Number(String(number)[1]))} ${read(Number(String(number)[2]))}`
       }
     }
-    
     
     function read(num){
     return String(arr.filter((el,index)=>{
