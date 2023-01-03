c4fa1033e6f8f42ad8f19e4d72e0f762f346d9c7
Faizieva Venera <v.faizyeva@gmail.com>
2023-01-03 16:35:24 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function toReadable (number) {
-  
-}
+
+    let arr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen','fourteen'
+    ,'fifteen', 'sixteen','seventeen','eighteen', 'nineteen']
+    let decade = ['twenty','thirty','forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let hundred = ['hundred']
+    
+      if (number==0){
+        return 'zero';
+      }else if(number<20){
+        return read(number);
+      }
+      else if(number>19 && number<100){
+        if(+String(number).slice(-1)==0){
+          return `${readDecade(Number(String(number)[0]))}`
+        }else{
+        return `${readDecade(Number(String(number)[0]))} ${read(Number(String(number)[1]))}`;
+      }
+      }else if(number>=100){
+        if(String(number).slice(-2)=='00'){
+        return `${read(Number(String(number)[0]))} hundred`;
+      }else if(+String(number).slice(1)<20){
+        return `${read(Number(String(number)[0]))} hundred ${read(+String(number).slice(1))}`
+      }else if(number>100 && +String(number).slice(-1)==0){
+        return `${read(Number(String(number)[0]))} hundred ${readDecade(Number(String(number)[1]))}`
+      }else{
+        return `${read(Number(String(number)[0]))} hundred ${readDecade(Number(String(number)[1]))} ${read(Number(String(number)[2]))}`
+      }
+    }
+    
+    
+    function read(num){
+    return String(arr.filter((el,index)=>{
+      if(index==num-1){
+        return el
+      }}))
+    }
+    function readDecade(num){
+      return String(decade.filter((el,index)=>{
+        if(index==num-2){
+          return el
+        }}))
+      }
+  }
